6da137daa52f5d1d52bf878ce7c63c46b3697f3b
Jason Farrell <imjason@gmail.com>
2018-07-16 11:22:53 -0500

    added request model to signature

--- AuthFunctions.cs
+++ AuthFunctions.cs
@@ -6,25 +6,18 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using AuthService.Models;
 
 namespace AuthService.Functions
 {
     public static class AuthFunctions
     {
         [FunctionName("perform_login")]
-        public static IActionResult PerformLogin([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult PerformLogin([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]LoginRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkResult();
         }
     }
 }
