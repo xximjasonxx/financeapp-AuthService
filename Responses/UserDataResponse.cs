f7666f588c7ee2c437896c501e8348684cf75f3e
Jason Farrell <imjason@gmail.com>
2018-07-17 11:50:43 -0500

    updated user responses

--- Responses/UserDataResponse.cs
+++ Responses/UserDataResponse.cs
@@ -4,15 +4,35 @@ using Newtonsoft.Json;
 
 namespace AuthService.Responses
 {
-    public class UserDataResponse : User
+    public class UserDataResponse
     {
-        [JsonIgnore]
-        public override string Password { get; set; }
+        [JsonProperty("emailAddress")]
+        public string EmailAddress { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("address1")]
+        public string Address1 { get; set; }
+
+        [JsonProperty("address2")]
+        public string Address2 { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("zipcode")]
+        public string PostalCode { get; set; }
 
         public UserDataResponse(User user)
         {
             this.EmailAddress = user.EmailAddress;
-            this.Password = user.Password;
             this.FirstName = user.FirstName;
             this.LastName = user.LastName;
             this.Address1 = user.Address1;
