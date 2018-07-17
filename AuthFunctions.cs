5fd72d6697ab5746bb9c03e79d2cf3a5e0f22f52
Jason Farrell <imjason@gmail.com>
2018-07-17 16:23:49 -0500

    Updated password handling

--- AuthFunctions.cs
+++ AuthFunctions.cs
@@ -45,7 +45,6 @@ namespace AuthService.Functions
         public static async Task<IActionResult> CreateUser([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]CreateUserRequest request, TraceWriter log)
         {
             // todo: add logic to check for duplicates
-            log.Info($"Email Address: {request.EmailAddress}");
             var newUser = new User(request);
 
             newUser = await UserService.CreateUser(newUser);
