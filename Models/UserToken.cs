c5030a98fbce5f855578bf4ced991679b1a41877
Jason Farrell <imjason@gmail.com>
2018-07-18 13:01:27 -0500

    usertoken needs _id to work, apparently

--- Models/UserToken.cs
+++ Models/UserToken.cs
@@ -1,11 +1,16 @@
 
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
 
 namespace AuthService.Models
 {
     public class UserToken
     {
+        [BsonId]
+        public ObjectId _Id { get; set; }
+
         [JsonProperty("token")]
         public string Token { get; set; }
 
