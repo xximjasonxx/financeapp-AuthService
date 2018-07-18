a96e4654a8474f6062bf19eae14e3cf9e5cc9b26
Jason Farrell <imjason@gmail.com>
2018-07-17 23:21:47 -0500

    super simple method

--- AuthFunctions.cs
+++ AuthFunctions.cs
@@ -73,19 +73,9 @@ namespace AuthService.Functions
         }
 
         [FunctionName("validate_token")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkResult();
         }
     }
 }
